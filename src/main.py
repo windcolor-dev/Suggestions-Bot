4f8b5a6ec9bed03be1e52aa75c988e0679fa7173
windcolor <teaglehao@gmail.com>
2021-05-23 20:59:23 -0700

    Added bot status switching

--- src/main.py
+++ src/main.py
@@ -1,9 +1,18 @@
 import discord
+import time
+
 # Replace this with your bot's token
 token = YourTokenHere
 
 # For information on color please see color-info.txt
-color = 0x3498db        
+color = 0x3498db     
+   
+# If the bot's status should be set (configure below)
+status_enabled = True
+
+# Replace this with what you want your bot's status to be shown as
+# This would set the bot's status to "Listening to !suggest"
+bot_status = '!suggest'
 
  # Replace this with your suggestions channel id
 suggestions_channel_id = YourSuggestionsChannelID
@@ -13,6 +22,12 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('The bot has logged in as {0.user}'.format(client)) 
+    await set_status()
+    
+async def set_status():
+  time.sleep(2)
+  if status_enabled == True:
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=bot_status))
 
 @client.event
 async def on_message(message):
